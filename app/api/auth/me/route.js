a61c940b60423ae918f50609e6f577f41b388679
Manoj <manojkarthick1706@gmail.com>
2024-04-06 17:57:17 +0530

    Fixed defualt import in me route

--- app/api/auth/me/route.js
+++ app/api/auth/me/route.js
@@ -1,4 +1,4 @@
-import getDataFromToken from "@/app/(helpers)/getDataFromToken";
+import { getDataFromToken } from "@/app/(helpers)/getDataFromToken";
 import { NextRequest, NextResponse } from "next/server";
 import User from "@/app/(models)/userModel";
 import { connect } from "@/app/(config)/dbConfig";
